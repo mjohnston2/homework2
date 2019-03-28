22be4816372c72b7b6affbbee52d4c626b939f7f
Mark <Mjstone587@yahoo.com>
2019-03-27 19:49:30 -0500

    first commit

---
+++ MJ_homework2.py
@@ -0,0 +1,7 @@
+#CIS 116 Homework 2
+
+numba_one = int(input("Please enter first number: "))
+numba_two = int(input("Please enter second number: "))
+
+if numba_one > numba_two:
+    print ("num 1 is bigger than num 2.")
