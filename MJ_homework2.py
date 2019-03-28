d932828a9a0460dbe5b02d6737fd9405be9a529d
Mark <Mjstone587@yahoo.com>
2019-03-27 19:50:37 -0500

    compare feature 1 less than 2

--- MJ_homework2.py
+++ MJ_homework2.py
@@ -5,3 +5,5 @@ numba_two = int(input("Please enter second number: "))
 
 if numba_one > numba_two:
     print ("num 1 is bigger than num 2.")
+if numba_one < numba_two:
+    print ("num 2 is bigger than num 1.")
